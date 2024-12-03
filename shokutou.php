3fd5b2b1a31738d5970a35e9e149db79a956fedb
五十嵐健太 <23jn0303@jec.ac.jp>
2024-12-03 16:39:42 +0900

    りきがコミット

--- shokutou.php
+++ shokutou.php
@@ -10,10 +10,7 @@
 </header>
 <body>
 <div class="header__inner"></div>
-<button id="js-hamburger" type="button" class="hamburger -active" aria-controls="navigation" aria-expanded="true"
-    aria-label="">
-    <span class="hamburger__line"></span>
-    <span class="hamburger__text"></span>
+<button id="js-hamburger">
 </button>
 <div class="header__nav-area js-nav-area -active" id="navigation">
     <nav id="js-global-navigation" class="global-navigation">
@@ -228,188 +225,189 @@
             </li>
         </ul>
         <div id="js-focus-trap" tabindex="0"></div>
+        
     </nav>  
-</div>
-<div class="choice">
-<div id="chk-veggie1" style="display: none;">
-    <h3>いつもの野菜:</h3>
-    <label><input type="checkbox" name="veggie" value="トマト"> トマト</label>
-    <label><input type="checkbox" name="veggie" value="キュウリ"> キュウリ</label><br>
-</div>
-<div id="chk-veggie2" style="display: none;">
-    <h3>葉物野菜:</h3>
-    <label><input type="checkbox" name="veggie" value="キャベツ"> キャベツ</label><br>
-    <label><input type="checkbox" name="veggie" value="キュウリ"> ほうれん草</label><br>
-</div>
-<div id="chk-veggie3" style="display: none;">
-    <h3>根菜類:</h3>
-    <label><input type="checkbox" name="veggie" value="トマト">根菜１</label><br>
-    <label><input type="checkbox" name="veggie" value="キュウリ">根菜２</label><br>
-</div>
-<div id="chk-veggie4" style="display: none;">
-    <h3>キノコ類:</h3>
-    <label><input type="checkbox" name="veggie" value="トマト"> きのこ１</label><br>
-    <label><input type="checkbox" name="veggie" value="キュウリ"> キノコ２</label><br>
-</div>
-<div id="chk-veggie5" style="display: none;">
-    <h3>果実野菜:</h3>
-    <label><input type="checkbox" name="veggie" value="トマト"> 果実野菜１</label><br>
-    <label><input type="checkbox" name="veggie" value="キュウリ"> 果実野菜２</label><br>
-</div>
-<div id="chk-veggie6" style="display: none;">
-    <h3>豆類:</h3>
-    <label><input type="checkbox" name="veggie" value="トマト"> 豆類１</label><br>
-    <label><input type="checkbox" name="veggie" value="キュウリ"> 豆類２</label><br>
-</div>
-<div id="chk-veggie7" style="display: none;">
-    <h3>その他:</h3>
-    <label><input type="checkbox" name="veggie" value="トマト"> その他１</label><br>
-    <label><input type="checkbox" name="veggie" value="キュウリ"> その他２</label><br>
-</div>
+    <div class="choice">
+        <div id="chk-veggie1" style="display: none;">
+            <h3>いつもの野菜:</h3>
+            <label><input type="checkbox" name="veggie" value="トマト"> トマト</label>
+            <label><input type="checkbox" name="veggie" value="キュウリ"> キュウリ</label><br>
+        </div>
+        <div id="chk-veggie2" style="display: none;">
+            <h3>葉物野菜:</h3>
+            <label><input type="checkbox" name="veggie" value="キャベツ"> キャベツ</label><br>
+            <label><input type="checkbox" name="veggie" value="キュウリ"> ほうれん草</label><br>
+        </div>
+        <div id="chk-veggie3" style="display: none;">
+            <h3>根菜類:</h3>
+            <label><input type="checkbox" name="veggie" value="トマト">根菜１</label><br>
+            <label><input type="checkbox" name="veggie" value="キュウリ">根菜２</label><br>
+        </div>
+        <div id="chk-veggie4" style="display: none;">
+            <h3>キノコ類:</h3>
+            <label><input type="checkbox" name="veggie" value="トマト"> きのこ１</label><br>
+            <label><input type="checkbox" name="veggie" value="キュウリ"> キノコ２</label><br>
+        </div>
+        <div id="chk-veggie5" style="display: none;">
+            <h3>果実野菜:</h3>
+            <label><input type="checkbox" name="veggie" value="トマト"> 果実野菜１</label><br>
+            <label><input type="checkbox" name="veggie" value="キュウリ"> 果実野菜２</label><br>
+        </div>
+        <div id="chk-veggie6" style="display: none;">
+            <h3>豆類:</h3>
+            <label><input type="checkbox" name="veggie" value="トマト"> 豆類１</label><br>
+            <label><input type="checkbox" name="veggie" value="キュウリ"> 豆類２</label><br>
+        </div>
+        <div id="chk-veggie7" style="display: none;">
+            <h3>その他:</h3>
+            <label><input type="checkbox" name="veggie" value="トマト"> その他１</label><br>
+            <label><input type="checkbox" name="veggie" value="キュウリ"> その他２</label><br>
+        </div>
 
 
 
 
-<div id="chk-fruits1" style="display: none;">
-    <h3>いつもの果物:</h3>
-    <label><input type="checkbox" name="fruits" value="トマト">いつもの果物１</label><br>
-    <label><input type="checkbox" name="fruits" value="キュウリ">いつもの果物２</label><br>
-</div>
-<div id="chk-fruits2" style="display: none;">
-    <h3>ドライフルーツ:</h3>
-    <label><input type="checkbox" name="fruits" value="トマト">ドライフルーツ１</label><br>
-    <label><input type="checkbox" name="fruits" value="キュウリ">ドライフルーツ２</label><br>
-</div>
-<div id="chk-fruits3" style="display: none;">
-    <h3>柑橘類:</h3>
-    <label><input type="checkbox" name="fruits" value="トマト">柑橘類</label><br>
-    <label><input type="checkbox" name="fruits" value="キュウリ">柑橘類</label><br>
-</div>
-<div id="chk-fruits4" style="display: none;">
-    <h3>ベリー類:</h3>
-    <label><input type="checkbox" name="fruits" value="トマト">ベリー類１</label><br>
-    <label><input type="checkbox" name="fruits" value="キュウリ">ベリー類２</label><br>
-</div>
-<div id="chk-fruits5" style="display: none;">
-    <h3>その他:</h3>
-    <label><input type="checkbox" name="fruits" value="トマト">その他１</label><br>
-    <label><input type="checkbox" name="fruits" value="キュウリ">その他２</label><br>
-</div>
+        <div id="chk-fruits1" style="display: none;">
+            <h3>いつもの果物:</h3>
+            <label><input type="checkbox" name="fruits" value="トマト">いつもの果物１</label><br>
+            <label><input type="checkbox" name="fruits" value="キュウリ">いつもの果物２</label><br>
+        </div>
+        <div id="chk-fruits2" style="display: none;">
+            <h3>ドライフルーツ:</h3>
+            <label><input type="checkbox" name="fruits" value="トマト">ドライフルーツ１</label><br>
+            <label><input type="checkbox" name="fruits" value="キュウリ">ドライフルーツ２</label><br>
+        </div>
+        <div id="chk-fruits3" style="display: none;">
+            <h3>柑橘類:</h3>
+            <label><input type="checkbox" name="fruits" value="トマト">柑橘類</label><br>
+            <label><input type="checkbox" name="fruits" value="キュウリ">柑橘類</label><br>
+        </div>
+        <div id="chk-fruits4" style="display: none;">
+            <h3>ベリー類:</h3>
+            <label><input type="checkbox" name="fruits" value="トマト">ベリー類１</label><br>
+            <label><input type="checkbox" name="fruits" value="キュウリ">ベリー類２</label><br>
+        </div>
+        <div id="chk-fruits5" style="display: none;">
+            <h3>その他:</h3>
+            <label><input type="checkbox" name="fruits" value="トマト">その他１</label><br>
+            <label><input type="checkbox" name="fruits" value="キュウリ">その他２</label><br>
+        </div>
 
 
 
 
-<div id="chk-meat1" style="display: none;">
-    <h3>牛肉:</h3>
-    <label><input type="checkbox" name="meat" value="トマト">牛肉１</label><br>
-    <label><input type="checkbox" name="meat" value="キュウリ">牛肉２</label><br>
-</div>
-<div id="chk-meat2" style="display: none;">
-    <h3>豚肉:</h3>
-    <label><input type="checkbox" name="meat" value="トマト"> 豚肉１</label><br>
-    <label><input type="checkbox" name="meat" value="キュウリ">豚肉２</label><br>
-</div>
-<div id="chk-meat3" style="display: none;">
-    <h3>鶏肉:</h3>
-    <label><input type="checkbox" name="meat" value="トマト"> 鶏肉１</label><br>
-    <label><input type="checkbox" name="meat" value="キュウリ">鶏肉２</label><br>
-</div>
-<div id="chk-meat4" style="display: none;">
-    <h3>肉加工品:</h3>
-    <label><input type="checkbox" name="meat" value="トマト">肉加工品１</label><br>
-    <label><input type="checkbox" name="meat" value="キュウリ"> 肉加工品２</label><br>
-</div>
-<div id="chk-meat5" style="display: none;">
-    <h3>その他:</h3>
-    <label><input type="checkbox" name="meat" value="トマト"> その他１</label><br>
-    <label><input type="checkbox" name="meat" value="キュウリ">その他２</label><br>
-</div>
-
-
-
-
-
-
-
-
-<div id="chk-fish1" style="display: none;">
-    <h3>青魚:</h3>
-    <label><input type="checkbox" name="fish" value="トマト"> 青魚１</label><br>
-    <label><input type="checkbox" name="fish" value="キュウリ"> 青魚２</label><br>
-</div>
-<div id="chk-fish2" style="display: none;">
-    <h3>赤身魚:</h3>
-    <label><input type="checkbox" name="fish" value="トマト">赤身魚１</label><br>
-    <label><input type="checkbox" name="fish" value="キュウリ"> 赤身魚２</label><br>
-</div>
-<div id="chk-fish3" style="display: none;">
-    <h3>白身魚:</h3>
-    <label><input type="checkbox" name="fish" value="トマト"> 白身魚１</label><br>
-    <label><input type="checkbox" name="fish" value="キュウリ"> 白身魚２</label><br>
-</div>
-<div id="chk-fish4" style="display: none;">
-    <h3>海藻類:</h3>
-    <label><input type="checkbox" name="fish" value="トマト"> 海藻類１</label><br>
-    <label><input type="checkbox" name="fish" value="キュウリ"> 海藻類２</label><br>
-</div>
-<div id="chk-fish5" style="display: none;">
-    <h3>魚加工品:</h3>
-    <label><input type="checkbox" name="fish" value="トマト"> 魚加工品１</label><br>
-    <label><input type="checkbox" name="fish" value="キュウリ"> 魚加工品２</label><br>
-</div>
-<div id="chk-fish6" style="display: none;">
-    <h3>その他:</h3>
-    <label><input type="checkbox" name="fish" value="トマト"> その他１</label><br>
-    <label><input type="checkbox" name="fish" value="キュウリ"> その他２</label><br>
-</div>
+        <div id="chk-meat1" style="display: none;">
+            <h3>牛肉:</h3>
+            <label><input type="checkbox" name="meat" value="トマト">牛肉１</label><br>
+            <label><input type="checkbox" name="meat" value="キュウリ">牛肉２</label><br>
+        </div>
+        <div id="chk-meat2" style="display: none;">
+            <h3>豚肉:</h3>
+            <label><input type="checkbox" name="meat" value="トマト"> 豚肉１</label><br>
+            <label><input type="checkbox" name="meat" value="キュウリ">豚肉２</label><br>
+        </div>
+        <div id="chk-meat3" style="display: none;">
+            <h3>鶏肉:</h3>
+            <label><input type="checkbox" name="meat" value="トマト"> 鶏肉１</label><br>
+            <label><input type="checkbox" name="meat" value="キュウリ">鶏肉２</label><br>
+        </div>
+        <div id="chk-meat4" style="display: none;">
+            <h3>肉加工品:</h3>
+            <label><input type="checkbox" name="meat" value="トマト">肉加工品１</label><br>
+            <label><input type="checkbox" name="meat" value="キュウリ"> 肉加工品２</label><br>
+        </div>
+        <div id="chk-meat5" style="display: none;">
+            <h3>その他:</h3>
+            <label><input type="checkbox" name="meat" value="トマト"> その他１</label><br>
+            <label><input type="checkbox" name="meat" value="キュウリ">その他２</label><br>
+        </div>
 
 
 
 
 
-<div id="chk-carbohy1" style="display: none;">
-    <h3>お米:</h3>
-    <label><input type="checkbox" name="carbohy" value="トマト"> お米１</label><br>
-    <label><input type="checkbox" name="carbohy" value="キュウリ"> お米２</label><br>
-</div>
-<div id="chk-carbohy2" style="display: none;">
-    <h3>パン:</h3>
-    <label><input type="checkbox" name="carbohy" value="トマト"> パン１</label><br>
-    <label><input type="checkbox" name="carbohy" value="キュウリ"> パン２</label><br>
-</div>
-<div id="chk-carbohy3" style="display: none;">
-    <h3>麺:</h3>
-    <label><input type="checkbox" name="carbohy" value="トマト"> 麺１</label><br>
-    <label><input type="checkbox" name="carbohy" value="キュウリ"> 麺２</label><br>
-</div>
-<div id="chk-carbohy4" style="display: none;">
-    <h3>その他:</h3>
-    <label><input type="checkbox" name="carbohy" value=""> その他１</label><br>
-    <label><input type="checkbox" name="carbohy" value="キュウリ"> その他２</label><br>
-</div>
 
 
-<div id="chk-egg1" style="display: none;">
-    <h3>卵:</h3>
-    <label><input type="checkbox" name="fish" value="トマト"> 卵１</label><br>
-    <label><input type="checkbox" name="fish" value="キュウリ"> 卵２</label><br>
-</div>
-<div id="chk-egg2" style="display: none;">
-    <h3>牛乳:</h3>
-    <label><input type="checkbox" name="fish" value="トマト">乳製品１</label><br>
-    <label><input type="checkbox" name="fish" value="キュウリ"> 乳製品２</label><br>
-</div>
 
-<div id="chk-soy" style="display: none;">
-    <h3>大豆製品:</h3>
-    <label><input type="checkbox" name="fish" value="トマト"> 大豆製品１</label><br>
-    <label><input type="checkbox" name="fish" value="キュウリ"> 大豆製品２</label><br>
-</div>
-</div>
+        <div id="chk-fish1" style="display: none;">
+            <h3>青魚:</h3>
+            <label><input type="checkbox" name="fish" value="トマト"> 青魚１</label><br>
+            <label><input type="checkbox" name="fish" value="キュウリ"> 青魚２</label><br>
+        </div>
+        <div id="chk-fish2" style="display: none;">
+            <h3>赤身魚:</h3>
+            <label><input type="checkbox" name="fish" value="トマト">赤身魚１</label><br>
+            <label><input type="checkbox" name="fish" value="キュウリ"> 赤身魚２</label><br>
+        </div>
+        <div id="chk-fish3" style="display: none;">
+            <h3>白身魚:</h3>
+            <label><input type="checkbox" name="fish" value="トマト"> 白身魚１</label><br>
+            <label><input type="checkbox" name="fish" value="キュウリ"> 白身魚２</label><br>
+        </div>
+        <div id="chk-fish4" style="display: none;">
+            <h3>海藻類:</h3>
+            <label><input type="checkbox" name="fish" value="トマト"> 海藻類１</label><br>
+            <label><input type="checkbox" name="fish" value="キュウリ"> 海藻類２</label><br>
+        </div>
+        <div id="chk-fish5" style="display: none;">
+            <h3>魚加工品:</h3>
+            <label><input type="checkbox" name="fish" value="トマト"> 魚加工品１</label><br>
+            <label><input type="checkbox" name="fish" value="キュウリ"> 魚加工品２</label><br>
+        </div>
+        <div id="chk-fish6" style="display: none;">
+            <h3>その他:</h3>
+            <label><input type="checkbox" name="fish" value="トマト"> その他１</label><br>
+            <label><input type="checkbox" name="fish" value="キュウリ"> その他２</label><br>
+        </div>
 
-<button id="">追加</button><br>
+
+
+
+
+        <div id="chk-carbohy1" style="display: none;">
+            <h3>お米:</h3>
+            <label><input type="checkbox" name="carbohy" value="トマト"> お米１</label><br>
+            <label><input type="checkbox" name="carbohy" value="キュウリ"> お米２</label><br>
+        </div>
+        <div id="chk-carbohy2" style="display: none;">
+            <h3>パン:</h3>
+            <label><input type="checkbox" name="carbohy" value="トマト"> パン１</label><br>
+            <label><input type="checkbox" name="carbohy" value="キュウリ"> パン２</label><br>
+        </div>
+        <div id="chk-carbohy3" style="display: none;">
+            <h3>麺:</h3>
+            <label><input type="checkbox" name="carbohy" value="トマト"> 麺１</label><br>
+            <label><input type="checkbox" name="carbohy" value="キュウリ"> 麺２</label><br>
+        </div>
+        <div id="chk-carbohy4" style="display: none;">
+            <h3>その他:</h3>
+            <label><input type="checkbox" name="carbohy" value=""> その他１</label><br>
+            <label><input type="checkbox" name="carbohy" value="キュウリ"> その他２</label><br>
+        </div>
+
+
+        <div id="chk-egg1" style="display: none;">
+            <h3>卵:</h3>
+            <label><input type="checkbox" name="fish" value="トマト"> 卵１</label><br>
+            <label><input type="checkbox" name="fish" value="キュウリ"> 卵２</label><br>
+        </div>
+        <div id="chk-egg2" style="display: none;">
+            <h3>牛乳:</h3>
+            <label><input type="checkbox" name="fish" value="トマト">乳製品１</label><br>
+            <label><input type="checkbox" name="fish" value="キュウリ"> 乳製品２</label><br>
+        </div>
+
+        <div id="chk-soy" style="display: none;">
+            <h3>大豆製品:</h3>
+            <label><input type="checkbox" name="fish" value="トマト"> 大豆製品１</label><br>
+            <label><input type="checkbox" name="fish" value="キュウリ"> 大豆製品２</label><br>
+        </div>
+        </div>
+        </div>
+<div class=toroku>
+<button id="tuika">追加</button><br>
 <div>
-<a href="satsuei.html"><img src="\\10.32.97.1\ProfilesRedirect$\23jn0303\Documents\G01_pro\G01\カメラ２.png" alt="カメラ画像" /></a>
+<a href="satsuei.php"><img src="カメラ２.png" alt="カメラ画像" /></a>
 </div>
 <p>↑撮影はこちら</p>
 
@@ -502,10 +500,10 @@
     </div>
     <!-- フッター部分 -->
     <div class="footer">
-      <a href="home.html" class="small-button">冷蔵庫in</a>
+      <a href="home.php" class="small-button">冷蔵庫in</a>
     </div>
   </div>
-<a href="home.html"><button>登録</button></a>
+</div>
 </body>
 
 <script src="shokutou.js"></script>
--- yomitori.php
+++ yomitori.php
@@ -10,7 +10,7 @@
     <div class="content-container">
         <!-- 画像を中央に配置 -->
         <div class="image-container">
-            <img src="\\10.32.97.1\ProfilesRedirect$\23jn0327\Documents\My Pictures\Screenshots\スクリーンショット 2024-11-19 104837.png" alt="サンプル画像" />
+            <img src="スクリーンショット 2024-11-19 104837.png" alt="サンプル画像" />
         </div>
 
         <!-- テキストボックスを画像の下に配置 -->
