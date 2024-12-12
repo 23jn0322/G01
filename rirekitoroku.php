66597596cd8c499668da0ba2d0ed1f7a6d34ed12
五十嵐健太 <23jn0303@jec.ac.jp>
2024-12-12 16:41:18 +0900

    komi

--- rirekitoroku.php
+++ rirekitoroku.php
@@ -1,19 +1,17 @@
 <?php 
-  require_once './helpers/RirekiDAO.php';
+  require_once './helpers/SyokutouDAO.php';
   require_once './helpers/MemberDAO.php';
 
-  SESSION_start();
+if(session_status() === PHP_SESSION_NONE){
+  session_start();
+}
 
-  if(!empty($SESSION['member'])){
-    header('Location:Login.php');
-    exit;
-  }
+  $Member = $_SESSION['Member'];
 
-  $member = $SESSION['member'];
-
-
-
+  $SyokutouDAO = new SyokutouDAO();
+  $rireki_list = $SyokutouDAO->get_syokutou_by_MID($Member->MID);
 ?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -23,87 +21,85 @@
   <link rel="stylesheet" href="css/rirekitoroku.css">
 </head>
 <body>
-  <div class="title">履歴から買い物登録</div>
-  <div class="container">
-    <div class="content">
-      <div class="items">
-        <div class="item-row">
-          <input type="text" class="item-input" placeholder="例: 牛肉" value="牛肉">
+<div class="title">履歴から買い物登録</div>
+    <div class="container">
+        <div class="content">
+            <div class="item-row">
+                <input type="text" class="item-input" placeholder="例: 牛肉" value="牛肉">
+                <div class="row">
+                    <input type="number" value="100" class="quantity-input">
+                    <select>
+                        <option value="g">g</option>
+                        <option value="kg">kg</option>
+                        <option value="本">本</option>
+                        <option value="玉">玉</option>
+                        <option value="個">個</option>
+                        <option value="束">束</option>
+                        <option value="袋">袋</option>
+                    </select>
+                </div>
+            </div>
+            <div class="item-row">
+                <input type="text" class="item-input" placeholder="例: ほうれん草" value="ほうれん草">
+                <div class="row">
+                    <input type="number" value="100" class="quantity-input">
+                    <select>
+                        <option value="g">g</option>
+                        <option value="kg">kg</option>
+                        <option value="本">本</option>
+                        <option value="玉">玉</option>
+                        <option value="個">個</option>
+                        <option value="束">束</option>
+                        <option value="袋">袋</option>
+                    </select>
+                </div>
+            </div>
+            <div class="item-row">
+                <input type="text" class="item-input" placeholder="例: にんじん" value="にんじん">
+                <div class="row">
+                    <input type="number" value="100" class="quantity-input">
+                    <select>
+                        <option value="g">g</option>
+                        <option value="kg">kg</option>
+                        <option value="本">本</option>
+                        <option value="玉">玉</option>
+                        <option value="個">個</option>
+                        <option value="束">束</option>
+                        <option value="袋">袋</option>
+                    </select>
+                </div>
+            </div>
+            <div class="item-row">
+                <input type="text" class="item-input" placeholder="例: 大根" value="大根">
+                <div class="row">
+                    <input type="number" value="100" class="quantity-input">
+                    <select>
+                        <option value="g">g</option>
+                        <option value="kg">kg</option>
+                        <option value="本">本</option>
+                        <option value="玉">玉</option>
+                        <option value="個">個</option>
+                        <option value="束">束</option>
+                        <option value="袋">袋</option>
+                    </select>
+                </div>
+            </div>
+            <div class="item-row">
+                <input type="text" class="item-input" placeholder="例: りんご" value="りんご">
+                <div class="row">
+                    <input type="number" value="100" class="quantity-input">
+                    <select>
+                        <option value="g">g</option>
+                        <option value="kg">kg</option>
+                        <option value="本">本</option>
+                        <option value="玉">玉</option>
+                        <option value="個">個</option>
+                        <option value="束">束</option>
+                        <option value="袋">袋</option>
+                    </select>
+                </div>
+            </div>
         </div>
-        <div class="item-row">
-          <input type="text" class="item-input" placeholder="例: ほうれん草" value="ほうれん草">
-        </div>
-        <div class="item-row">
-          <input type="text" class="item-input" placeholder="例: にんじん" value="にんじん">
-        </div>
-        <div class="item-row">
-            <input type="text" class="item-input" placeholder="例: 大根" value="大根">
-          </div>
-        <div class="item-row">
-            <input type="text" class="item-input" placeholder="例: りんご" value="りんご">
-          </div>
-      </div>
-      <div class="quantities">
-        <div class="row">
-          <input type="number" value="100" class="quantity-input">
-          <select>
-            <option value="g">g</option>
-            <option value="kg">kg</option>
-            <option value="本">本</option>
-            <option value="玉">玉</option>
-            <option value="個">個</option>
-            <option value="束">束</option>
-            <option value="袋">袋</option>
-          </select>
-        </div>
-        <div class="row">
-          <input type="number" value="2" class="quantity-input">
-          <select>
-            <option value="g">g</option>
-            <option value="kg">kg</option>
-            <option value="本">本</option>
-            <option value="玉">玉</option>
-            <option value="個">個</option>
-            <option value="束">束</option>
-            <option value="袋">袋</option>
-          </select>
-        </div>
-        <div class="row">
-          <input type="number" value="1" class="quantity-input">
-          <select>
-            <option value="g">g</option>
-            <option value="kg">kg</option>
-            <option value="本">本</option>
-            <option value="玉">玉</option>
-            <option value="個">個</option>
-            <option value="束">束</option>
-            <option value="袋">袋</option>
-          </select>
-        </div>
-        <div class="row">
-            <input type="number" value="1" class="quantity-input">
-            <select>
-              <option value="g">g</option>
-            <option value="kg">kg</option>
-            <option value="本">本</option>
-            <option value="玉">玉</option>
-            <option value="個">個</option>
-            <option value="束">束</option>
-            <option value="袋">袋</option>
-            </select>
-          </div>  <div class="row">
-            <input type="number" value="3" class="quantity-input">
-            <select>
-              <option value="g">g</option>
-            <option value="kg">kg</option>
-            <option value="本">本</option>
-            <option value="玉">玉</option>
-            <option value="個">個</option>
-            <option value="束">束</option>
-            <option value="袋">袋</option>
-            </select>
-          </div>
-      </div>
     </div>
     <!-- フッター部分 -->
     <div class="footer">
