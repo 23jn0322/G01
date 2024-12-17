6e67a54445f79b73692f453b983e4b9a846b0698
五十嵐健太 <23jn0303@jec.ac.jp>
2024-12-17 14:09:38 +0900

    RirekiDAOにUID指定追加

--- helpers/RirekiDAO.php
+++ helpers/RirekiDAO.php
@@ -3,13 +3,17 @@ require_once 'DAO.php';
 require_once 'FoodsDAO.php';
 require_once 'MemberDAO.php';
 
-class rireki{
-   public string $syokuname;
+class Rireki{
+   public string $SyokuName;
    public string $MID;
+}
+class Unit{
+    public string $UID;
+    public string $SyokuID;
 }
 
 class rirekiDAO{
-    public function get_rireki_by_syokuID(string $syokuID,string $MID)
+    public function get_rireki_by_syokuID(string $MID)
     {
         $dbh = DAO::get_db_connect();
 
@@ -20,7 +24,26 @@ class rirekiDAO{
 
         $stmt = $dbh->prepare($sql);
 
-        $stmt->bindValue(':SyokuName',$syokuname,PDO::PARAM_STR);
+        $stmt->bindValue(':MID',$MID,PDO::PARAM_STR);
+        $stmt->execute();
+
+        $data = [];
+        while($row = $stmt->fetchObject('Rireki')){
+        $data[] = $row;
+    }
+    return $data;
+    }
+    public function get_rireki_by_UID(string $MID)
+    {
+        $dbh = DAO::get_db_connect();
+
+        $sql = "select DISTINCT Foods.SyokuName,syokutou.MID 
+        from Foods 
+        INNER JOIN syokutou ON Foods.SyokuID = syokutou.SyokuID
+         where syokutou.MID = :MID";
+
+        $stmt = $dbh->prepare($sql);
+
         $stmt->bindValue(':MID',$MID,PDO::PARAM_STR);
         $stmt->execute();
 
--- rirekitoroku.php
+++ rirekitoroku.php
@@ -1,51 +1,56 @@
-<?php 
-  require_once './helpers/SyokutouDAO.php';
-  require_once './helpers/MemberDAO.php';
-  require_once './helpers/RirekiDAO.php';
+<?php
+require_once './helpers/SyokutouDAO.php';
+require_once './helpers/MemberDAO.php';
+require_once './helpers/RirekiDAO.php';
 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+if (!empty($_SESSION['Member'])) {
+    $Member = $_SESSION['Member'];
+}
 
-  session_start();
-
-
-  $RirekiDAO = new RirekiDAO();
-  $rireki_list = $RirekiDAO->get_rireki_by_syokuID($syokutou->syokuID);
+$RirekiDAO = new RirekiDAO();
+$rireki_list = $RirekiDAO->get_rireki_by_syokuID($Member->MID);
 ?>
 
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>買い物登録画面</title>
-  <link rel="stylesheet" href="css/rirekitoroku.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>買い物登録画面</title>
+    <link rel="stylesheet" href="css/rirekitoroku.css">
 </head>
+
 <body>
-<div class="title">履歴から買い物登録</div>
+    <div class="title">履歴から買い物登録</div>
     <div class="container">
         <div class="content">
             <div class="item-row">
-                <?php foreach($rireki_list as $MID) : ?>
-                <input type="text" class="item-input" value="<?=$Foods->SyokuName?>">
-                <div class="row">
-                    <input type="number" value="100" class="quantity-input">
-                    <select>
-                        <option value="g">g</option>
-                        <option value="kg">kg</option>
-                        <option value="本">本</option>
-                        <option value="玉">玉</option>
-                        <option value="個">個</option>
-                        <option value="束">束</option>
-                        <option value="袋">袋</option>
-                    </select>
-                </div>
+                <?php foreach ($rireki_list as $rireki) : ?>
+                    <input type="text" class="item-input" value="<?= $rireki->SyokuName ?>">
+                    <div class="row">
+                        <input type="number" value="100" class="quantity-input">
+                        <select>
+                            <option value="g">g</option>
+                            <option value="kg">kg</option>
+                            <option value="本">本</option>
+                            <option value="玉">玉</option>
+                            <option value="個">個</option>
+                            <option value="束">束</option>
+                            <option value="袋">袋</option>
+                        </select>
+                    </div>
+                <?php endforeach ?>
             </div>
         </div>
     </div>
-    <?php endforeach?>
     <!-- フッター部分 -->
     <div class="footer">
-      <a href="home.php" class="small-button">冷蔵庫in</a>
+        <a href="home.php" class="small-button">冷蔵庫in</a>
     </div>
-  </div>
 </body>
+
 </html>
